322eb086a760e20e2f54612b01efee35ee821d81
Макс <yashanty@gmail.com>
2021-09-15 22:06:49 +0300

    изменено view для отображения данных из базы

---
+++ Controllers/FamilyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp_net_core_mvc1.Data;
+using asp_net_core_mvc1.Models;
+
+namespace asp_net_core_mvc1.Controllers
+{
+    public class FamilyController : Controller
+    {
+        private readonly ApplicationDBContext db;
+        public FamilyController(ApplicationDBContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Family> familyList = db.Persons;
+
+            return View(familyList);
+        }
+    }
+}
