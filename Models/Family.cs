8d08b072ba2739a57444d86002e63d6eb78b33ad
Макс <yashanty@gmail.com>
2021-09-21 21:49:58 +0300

    add to view custom column titles used model attribute for Name to show in view

--- Models/Family.cs
+++ Models/Family.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace asp_net_core_mvc1.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Как величать!")]//если не прописывать во вьюшке название лейбла, то можно использовать атрибут в модели
         public string Name { get; set; }
         public byte Age { get; set; }
         public string Gender { get; set; }
